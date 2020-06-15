4b841fc62075af364e825384214ea35463061ee8
Manish Raghav <manishraghav9733@gmail.com>
2020-06-15 14:16:33 +0530

    adding a new branch

--- src/components/dashboard/detailCard.jsx
+++ src/components/dashboard/detailCard.jsx
@@ -1,16 +1,19 @@
 import React,{useState} from 'react';
-import { Route, Redirect } from "react-router-dom";
 import { Card, Col, Row } from 'antd';
+import index from './index'
 
 
 const card = (props) => { 
-
     return(
-        <div>
-          <Card title="client" extra={<a href="#">More</a>} style={{ width: 300 }}>
-            <h5>3 clients</h5>
+    <div className="site-card-wrapper">
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title="{props.title}" bordered={false}>
+            <p>{props.value}</p>
           </Card>
-        </div>
+        </Col>
+        </Row>
+    </div>,
       );
 }
 export default card;
--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Card, Col, Row } from 'antd';
 
-const App =()=>{
+const dashboardIndex =()=>{
     return(
         <div className="site-card-wrapper">
         <Row gutter={16}>
           <Col span={8}>
             <Card>
-            <detailCard title='clients' value='5' />
+            <card title='clients' value='5' />
             </Card>
           </Col>
           </Row>
       </div>
     )
 };
-export default App;
+export default dashboardIndex;
