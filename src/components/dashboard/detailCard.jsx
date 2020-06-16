c622c1fef2d67c4403df64e0bf5849e2dafdbaf6
Manish Raghav <manishraghav9733@gmail.com>
2020-06-16 14:26:26 +0530

    detail card

--- src/components/dashboard/detailCard.jsx
+++ src/components/dashboard/detailCard.jsx
@@ -1,19 +1,53 @@
 import React,{useState} from 'react';
 import { Card, Col, Row } from 'antd';
-import dashboardIndex from './dashboardIndex
 
+const detailCard = (props) => {
 
-const detailCard = (props) => { 
-    return(
+  const cardDetail=[{
+  Name :"Current Client",
+  Value:10
+  },
+{
+  Name :"Intrested Client",
+  Value:45
+},
+{
+   Name:"Profile Sent",
+  Value:23
+},
+{
+  Name:"Today's Leads",
+  Value:43
+},
+{
+  Name:"Last Week Leads",
+  Value:46
+}
+,
+{
+  Name:"Last Month Leads",
+  Value:43
+};
+];
+ return(
     <div className="site-card-wrapper">
       <Row gutter={16}>
         <Col span={8}>
-          <Card title="{props.title}" bordered={false}>
-            <p>{props.value}</p>
+          <Card title="Card title" bordered={false}>
+            Card content
           </Card>
         </Col>
-        </Row>
-    </div>,
-      );
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            Card content
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            Card content
+          </Card>
+        </Col>
+      </Row>
+    </div>
 }
 export default detailCard;
